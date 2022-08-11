2c703a2ddaaa187b5d14109a3b09887ffe93b441
Ryan Wade <ryanwadedevelopment@gmail.com>
2022-08-11 05:56:20 -0500

    Added shopping_list and child component

---
+++ src/app/shopping_list/shopping_list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping_list.component.html',
+  styleUrls: ['./shopping_list.component.css'],
+})
+export class ShoppingListComponent {}
---
+++ src/app/shopping_list/shopping_list_edit/shopping_list_edit.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shopping-edit',
+  templateUrl: './shopping_list_edit.component.html',
+  styleUrls: ['./shopping_list_edit.component.css'],
+})
+export class ShoppingListEditComponent {}
