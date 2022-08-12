274605db18daa977be217e9ac841c0b7c78061e6
Ryan Wade <ryanwadedevelopment@gmail.com>
2022-08-11 20:40:08 -0500

    Video 55

--- src/app/recipe_book/recipe_list/recipe_list.component.ts
+++ src/app/recipe_book/recipe_list/recipe_list.component.ts
@@ -13,5 +13,10 @@ export class RecipeListComponent {
       'This is a test',
       'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2Fa3fb2fb40f7208d148aed9ec2de15c8c%2F165576035120220620_172444.jpg&w=595&h=398&c=sc&poi=face&q=60'
     ),
+    new Recipe(
+      'Test2',
+      'This is a test2',
+      'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fpublic-assets.meredithcorp.io%2Fa3fb2fb40f7208d148aed9ec2de15c8c%2F165576035120220620_172444.jpg&w=595&h=398&c=sc&poi=face&q=60'
+    ),
   ];
 }
